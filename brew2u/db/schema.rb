f173fade65e670b51455a9e1ce971d0e542c9c9b
Ubuntu <ubuntu@ip-172-31-30-137.us-east-2.compute.internal>
2019-06-17 22:36:56 +0000

    added columns to devise admin

--- brew2u/db/schema.rb
+++ brew2u/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_221110) do
+ActiveRecord::Schema.define(version: 2019_06_17_222108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2019_06_17_221110) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "street_1"
+    t.string "street_2"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
