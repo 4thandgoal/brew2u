9b482e62f10699dee4138bab565ae059d88a716f
Ubuntu <ubuntu@ip-172-31-30-137.us-east-2.compute.internal>
2019-06-18 17:04:14 +0000

    syntax changed line 9 of routes.rb

--- brew2u/config/routes.rb
+++ brew2u/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     devise_for :admins
     devise_for :users
     
-    get '*path', :to => 'pages#root', constraints: ->(request){ request.format.html? }
+    get '*path', to: 'pages#root', constraints: ->(request){ request.format.html? }
     
     root to: 'pages#index'
 end
