628bc4b8cf56ac250beb21444af4a2eefcefd79c
Ubuntu <ubuntu@ip-172-31-20-95.us-east-2.compute.internal>
2019-06-18 19:38:01 +0000

    added 3 users and 3 admins

--- brew2u/db/seeds.rb
+++ brew2u/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2.times do
+user = User.create( 
+    {
+        email: Faker::Internet.email,
+        password: "asdfsafd",
+        password_confirmation: "asdfsafd",
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        date_of_birth: Faker::Date.birthday(8, 100),
+        phone: Faker::PhoneNumber.cell_phone,
+        street_1: Faker::Address.street_address,
+        street_2: Faker::Address.secondary_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        zip: Faker::Address.zip_code
+    })
+    
+admin = Admin.create(
+    {   
+        email: Faker::Internet.email,
+        password: "asdfsafd",
+        password_confirmation: "asdfsafd",
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        phone: Faker::PhoneNumber.cell_phone,
+        street_1: Faker::Address.street_address,
+        street_2: Faker::Address.secondary_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        zip: Faker::Address.zip_code
+    })    
+end
+     
