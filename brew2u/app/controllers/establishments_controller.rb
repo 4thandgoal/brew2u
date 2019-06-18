885b159081ee7b16efca440557ed7d3da384bf95
Ubuntu <ubuntu@ip-172-31-29-136.us-east-2.compute.internal>
2019-06-18 18:53:27 +0000

    includes methods in controller

--- brew2u/app/controllers/establishments_controller.rb
+++ brew2u/app/controllers/establishments_controller.rb
@@ -1,3 +1,31 @@
 class EstablishmentsController < ApplicationController
-    before_action : set_establishment, 
+    # protect all establishment routes
+    before_action :set_establishment, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
+    
+    def index
+        establishments = Establishment.all
+        render json: establishments, status: 200
+    end
+    
+    def create
+        establishment = current_admin.establishments.new(establishment_params)
+        if establishment.save
+            render json: establishment, status: 201
+        else 
+            # A 422 status code occurs when a request is well-formed, however, due to semantic errors it is unable to be processed.
+            render json: establishment.errors.full_message, status: 422
+        end
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+    def establishment_params
+        params.require(:establishment).permit(admin_id, company_name)
+    end
 end
