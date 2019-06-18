62dbeb0265a375ae086aa55e9391d0341a488dda
Ubuntu <ubuntu@ip-172-31-43-189.us-east-2.compute.internal>
2019-06-18 20:51:56 +0000

    Began work on the navigation

--- brew2u/app/javascript/components/Routes.js
+++ brew2u/app/javascript/components/Routes.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import App from './App';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 class Routes extends Component {
+  constructor() {
+    super()
+  
+  }
+  
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
     
   render() {
     return (
       <React.Fragment>
+        <NavBar>
+        </NavBar>
         <Switch>
         </Switch>
       </React.Fragment>
