8d86283ab0e1777fba56a2960fe7d77b8d654c01
Ubuntu <ubuntu@ip-172-31-43-189.us-east-2.compute.internal>
2019-06-18 17:42:08 +0000

    Initial commit of Routes piece in order to queue up pull request

---
+++ brew2u/app/javascript/components/Routes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import App from './App';
+
+class Routes extends Component {
+    
+  render() {
+    return (
+      <div>
+        <Switch>
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default Routes;    
+    
