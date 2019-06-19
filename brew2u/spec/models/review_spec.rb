90136e8cba5318eb73a84117866e7d19a3eb7104
Ubuntu <ubuntu@ip-172-31-30-155.us-east-2.compute.internal>
2019-06-19 21:59:07 +0000

    created model tests

--- brew2u/spec/models/review_spec.rb
+++ brew2u/spec/models/review_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+require 'rails_helper'
+
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it "should validate rating" do
+       review = Review.create
+       expect(review.errors[:rating]).to_not be_empty
+    end
+
+   it "should validate review" do
+       review = Review.create
+       expect(review.errors[:review]).to_not be_empty
+    end
 end
--- brew2u/spec/models/user_spec.rb
+++ brew2u/spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it "should validate email" do
+       user = User.create
+       expect(user.errors[:email]).to_not be_empty
+    end
+
+   it "should validate encrypted_password" do
+       user = User.create
+       expect(user.errors[:encrypted_password]).to_not be_empty
+    end
+    
+     it "should validate first_name" do
+       user = User.create
+       expect(user.errors[:first_name]).to_not be_empty
+    end
+    
+    it "should validate last_name" do
+       user = User.create
+       expect(user.errors[:last_name]).to_not be_empty
+    end
+    
+    it "should validate date_of_birth" do
+       user = User.create
+       expect(user.errors[:date_of_birth]).to_not be_empty
+    end
+    
+    it "should validate phone" do
+       user = User.create
+       expect(user.errors[:phone]).to_not be_empty
+    end
+    
+    it "should validate street_1" do
+       user = User.create
+       expect(user.errors[:street_1]).to_not be_empty
+    end
+    
+    it "should validate street_2" do
+       user = User.create
+       expect(User.errors[:street_2]).to_not be_empty
+    end
+    
+    it "should validate city" do
+       user = User.create
+       expect(user.errors[:city]).to_not be_empty
+    end
+    
+    it "should validate state" do
+       user = User.create
+       expect(user.errors[:state]).to_not be_empty
+    end
+    
+    it "should validate zip" do
+       user = User.create
+       expect(user.errors[:zip]).to_not be_empty
+    end
 end
