160a7c32a85fe1b0ba2bd6a529a21388ec3f0662
Ubuntu <ubuntu@ip-172-31-20-95.us-east-2.compute.internal>
2019-06-20 18:27:26 +0000

    jest tests not functioning, added line to json file line 32, can't run tests

--- brew2u/app/javascript/components/App.js
+++ brew2u/app/javascript/components/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <h2>whatever</h2>
         <Router>
           <Routes />
         </Router>
---
+++ brew2u/app/javascript/components/tests/App.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Landing from './Landing'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+import Routes from './Routes'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+it('should display an introduction message', () => {
+    const landing = mount(<Landing />)
+    expect(landing.find('#intro').text()).toMatch(/\w/i)
+})
+
+it('gives a message of title page name', ()=>{
+  const app = mount(<App />)
+  expect(app.find('h2').text()).toEqual('Brew2U')
+})
+ 
+it('gives text of navbar', ()=>{
+  const app = mount(<Routes />)
+  expect(app.find('h1').text()).toEqual('navbar')
+})
+ 
+it('Landing renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Landing />, div)
+})
+// it("renders an image", () => {
+//         const logo = shallow(<Logo />);
+//         expect(logo.find("img").prop("src")).toEqual(logoImage);
+// });
+ it("becomes active when the button is clicked", () => {
+  const wrapper = shallow(<Routes />);
+  wrapper.find(".open-menu-button-class").simulate("click");
+  expect(wrapper.state("active")).toBeTruthy();
+})
+ 
+ it("renders options when active", () => {
+  const wrapper = shallow(<Routes />);
+  wrapper.setState({ active: true });
+  expect(wrapper.find(".option").length).toBe(2);
+})
