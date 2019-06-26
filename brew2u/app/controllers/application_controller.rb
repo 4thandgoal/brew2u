87e40b6a807effefd62c62e4697b7c8845c91a49
Ubuntu <ubuntu@ip-172-31-43-189.us-east-2.compute.internal>
2019-06-26 23:39:09 +0000

    Fixed devise forms per Matt C.

--- brew2u/app/controllers/application_controller.rb
+++ brew2u/app/controllers/application_controller.rb
@@ -1,20 +1,5 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-    
-    layout :layout_by_resource
-    
-    private
-    
-    def layout_by_resource
-        if devise_controller? && resource_name == :user
-            "user_layout"
-        elsif devise_controller? && resource_name == :admin
-            "admin_layout"
-        else
-            "application"
-        end
-    end
-    
 end
 
 
