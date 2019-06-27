263eecc5b6c5e262f3f1c576f336824ef4562ed6
Ubuntu <ubuntu@ip-172-31-30-137.us-east-2.compute.internal>
2019-06-27 16:00:35 +0000

    revised address method

--- brew2u/app/models/establishment.rb
+++ brew2u/app/models/establishment.rb
@@ -6,7 +6,16 @@ class Establishment < ApplicationRecord
   validates :company_name, :coffee_or_beer, :phone, :website, :street_1, :city, :state, :zip, :hours_of_operation, :pet_friendly, :wifi, presence: true
   validates :zip, numericality: { only_integer: true }
   
+  def address
+    if :street_2 =~ /\d/ || :street_2 /\w/
+      [:street_1, :street_2, :city, :state, :zip].compact.join(', ')
+    else
+      [:street_1, :city, :state, :zip].compact.join(', ')
+    end
+  end
+  
   geocoded_by :address
   after_validation :geocode
+  
 
 end
