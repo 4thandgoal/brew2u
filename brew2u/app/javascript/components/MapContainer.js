6489b42006815d5ce58ceb6a0edb86bc781dfe77
Ubuntu <ubuntu@ip-172-31-20-95.us-east-2.compute.internal>
2019-06-28 00:58:40 +0000

    map with working location and marker

--- brew2u/app/javascript/components/MapContainer.js
+++ brew2u/app/javascript/components/MapContainer.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 
-export class MapContainer extends React.Component {
+class MapContainer extends React.Component {
    constructor(props){
     super(props)
      this.state = {
@@ -27,22 +27,24 @@ export class MapContainer extends React.Component {
     }
   }
    
+   
+   
   render () {
     const mapStyles = {
       width: "50%",
       height: "50%",
     }
-
-    const { name, latitude, longitude } = this.props
-
+      
+    const { google, name, latitude, longitude } = this.props
+    console.log(this.props)
     return (
       <React.Fragment>
         <Map
-          google={this.props.google}
+          google={google}
           style={mapStyles}
-          center={{
-            lat: {latitude},
-            lng: {longitude}
+          initialCenter={{
+            lat: 32.892507,
+            lng: -117.144781
           }}
           zoom={15}
           onClick={this.onMapClicked}
@@ -51,10 +53,9 @@ export class MapContainer extends React.Component {
 
             name={name} 
             title={name}
-            position = {{lat: {latitude}, lng: {longitude}}}
+            position = {{lat: 32.892507, lng: -117.144781}}
           />
-          {console.log(name)}
-
+        
           <InfoWindow
             marker={this.state.activeMarker}
             visible={this.state.showingInfoWindow}
@@ -70,5 +71,5 @@ export class MapContainer extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyDSkERfUbqV1nPReiTvz2thgnd1cEgA_sk")
+  apiKey: "AIzaSyDSkERfUbqV1nPReiTvz2thgnd1cEgA_sk"
 })(MapContainer)
