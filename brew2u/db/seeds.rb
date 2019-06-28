89e215f5e7d009f24ea731ddf2ee1762a4c0e9ed
Ubuntu <ubuntu@ip-172-31-20-95.us-east-2.compute.internal>
2019-06-28 19:24:24 +0000

    added faker to seed reviews database

--- brew2u/db/seeds.rb
+++ brew2u/db/seeds.rb
@@ -6,41 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 2.times do
-user = User.create( 
-    {
-        email: Faker::Internet.email,
-        password: "asdfsafd",
-        password_confirmation: "asdfsafd",
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        date_of_birth: Faker::Date.birthday(8, 100),
-        phone: Faker::PhoneNumber.cell_phone,
-        street_1: Faker::Address.street_address,
-        street_2: Faker::Address.secondary_address,
-        city: Faker::Address.city,
-        state: Faker::Address.state,
-        zip: Faker::Address.zip_code
-    })
+# user = User.create( 
+#     {
+#         email: Faker::Internet.email,
+#         password: "asdfsafd",
+#         password_confirmation: "asdfsafd",
+#         first_name: Faker::Name.first_name,
+#         last_name: Faker::Name.last_name,
+#         date_of_birth: Faker::Date.birthday(8, 100),
+#         phone: Faker::PhoneNumber.cell_phone,
+#         street_1: Faker::Address.street_address,
+#         street_2: Faker::Address.secondary_address,
+#         city: Faker::Address.city,
+#         state: Faker::Address.state,
+#         zip: Faker::Address.zip_code
+#     })
     
-admin = Admin.create(
-    {   
-        email: Faker::Internet.email,
-        password: "asdfsafd",
-        password_confirmation: "asdfsafd",
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        phone: Faker::PhoneNumber.cell_phone,
-        street_1: Faker::Address.street_address,
-        street_2: Faker::Address.secondary_address,
-        city: Faker::Address.city,
-        state: Faker::Address.state,
-        zip: Faker::Address.zip_code
-    }) 
+# admin = Admin.create(
+#     {   
+#         email: Faker::Internet.email,
+#         password: "asdfsafd",
+#         password_confirmation: "asdfsafd",
+#         first_name: Faker::Name.first_name,
+#         last_name: Faker::Name.last_name,
+#         phone: Faker::PhoneNumber.cell_phone,
+#         street_1: Faker::Address.street_address,
+#         street_2: Faker::Address.secondary_address,
+#         city: Faker::Address.city,
+#         state: Faker::Address.state,
+#         zip: Faker::Address.zip_code
+#     }) 
     
 review = Review.create(
     {
-        rating: 3,
-        review: "The coffee is really strong and bitter."
+        user_id: 1,
+        establishment_id: 1,
+        rating: Faker::Number.between(1, 5),
+        review: Faker::Movies::HarryPotter.quote
     })   
 end
      
