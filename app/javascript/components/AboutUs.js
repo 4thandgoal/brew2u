9828e7e84488e1e6fe5295ce63617e276f84521b
Ubuntu <ubuntu@ip-172-31-29-136.us-east-2.compute.internal>
2019-07-02 05:44:06 +0000

    added team names and profile

--- app/javascript/components/AboutUs.js
+++ app/javascript/components/AboutUs.js
@@ -10,13 +10,29 @@ class AboutUs extends React.Component {
             <Container>
               <Row>
                 <Col xs={6} md={4}>
-                  <Image src="Joel Magalin.jpg" rounded />
+                  <img src="/assets/images/headshots/joelMagsalin.jpg" rounded />
+                  <h5>Matt Au</h5>
+                  <p>After a top secret Super-Soldier program transformed frail Steve Rogers (Chris Evans) into the powerful and heroic Captain America, his amazing WWII exploits made him a living legend. Steve saved New York City and turned the tide of the war, but crashed into the Arctic during his final mission. Awakening in the modern day, Steve learned that he had spent decades frozen in the icy tundra. Now, Steve Rogers finds himself alone in a modern world that he hardly recognizes. When Nick Fury, director of the international peacekeeping agency S.H.I.E.L.D.</p>
                 </Col>
                 <Col xs={6} md={4}>
-                  <Image src="holder.js/171x180" roundedCircle />
+                  <img src="/assets/images/headshots/joelMagsalin.jpg" rounded />
+                  <h5>Joel Magsalin</h5>
+                  <p>On top of being an eccentric genius, a billionaire, a playboy and a philanthropist, Tony Stark (Robert Downey Jr.) is also the armored super hero known as Iron Man. Fresh off of defeating enemies the world over, Stark reluctantly agreed to serve as a consultant to Nick Fury’s top-secret peacekeeping and intelligence agency known as S.H.I.E.L.D. Now, with a global crisis on the horizon and the fate of the world in the balance, Stark must again power up his Iron Man armor to save the world, and become a full-fledged member of The Avengers. </p>
                 </Col>
                 <Col xs={6} md={4}>
-                  <Image src="holder.js/171x180" thumbnail />
+                  <img src="/assets/images/headshots/joelMagsalin.jpg" rounded />
+                  <h5>Gina Verrastro</h5>
+                  <p>S.H.I.E.L.D. Agent Natasha Romanoff, aka Black Widow (Scarlett Johansson), is one of the world’s greatest spies and quite possibly the world’s most skilled assassin. Armed with an extensive arsenal of specialized weapons like widow stings and cluster bombs, as well as a vast repertoire of martial arts skills, Romanoff’s unique talents make her one of director Fury’s top agents and an integral part in assembling The Avengers. </p>
+                </Col>
+                <Col xs={6} md={4}>
+                  <img src="/assets/images/headshots/joelMagsalin.jpg" rounded />
+                  <h5>Mike Acio</h5>
+                  <p>After a gamma radiation experiment went awry, mild-mannered scientist Dr. Bruce Banner (Mark Ruffalo) found himself with a peculiar condition. When angered or provoked, he would transform into the uncontrollable, green-skinned monster known as the Hulk. Now, fearful of the damage that the Hulk could inflict, Dr. Banner chooses to live a discreet life in remote parts of the world, working to cure the sick and help the poor while trying to elude those who would take advantage of his ability to change into the enormous, green menace. When a mounting threat calls for Banner’s specialized scientific expertise, S.H.I.E.L.D. Director Nick Fury recruits him, knowing full well that the incredible strength of Banner’s alter ego, the Hulk, would be an asset to The Avengers.</p>
+                </Col>
+                <Col xs={6} md={4}>
+                  <img src="/assets/images/headshots/joelMagsalin.jpg" rounded />
+                  <h5>Sunil Karakkattilrajappan</h5>
+                  <p>An arrogant prince from the distant land of Asgard, Thor (Chris Hemsworth) was banished to Earth after his irresponsible behavior threatened his homeland. While in exile on Earth, Thor learned humility and helped to save his new friends from a destructive threat sent by his brother Loki. In the process, Thor redeemed himself in the eyes of his father, Odin, the King of Asgard. After being welcomed back to Asgard as a hero, Thor must now return to Earth once again to prevent a cosmic-level catastrophe. With Mjolnir in his hand, a legendary hammer with immense power, the mighty warrior soon finds himself drawn into an unlikely alliance with Nick Fury’s secret initiative, The Avengers, lending his power to their cause against his wayward brother, Loki. </p>
                 </Col>
               </Row>
             </Container>
